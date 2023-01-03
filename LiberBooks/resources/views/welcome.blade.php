07649a1c57f5b69d583973e02c4f618c35f68cc1
sacred142 <agung.alfatah43@gmail.com>
2023-01-04 05:47:27 +0700

    menambahkan link

--- LiberBooks/resources/views/welcome.blade.php
+++ LiberBooks/resources/views/welcome.blade.php
@@ -321,16 +321,16 @@
         <div class="flex-1 bg-white p-6 flex flex-col justify-between">
           <div class="flex-1">
             <p class="text-sm font-medium text-cyan-600">
-              <a href="/allbooks?category={{ $b->category->name }}" class="hover:underline">{{ $b->category->name }}</a>
+              <a href="/allbooks?category={{ $b->category->name }}" class="hover:underline hover:text-amber-900">{{ $b->category->name }}</a>
             </p>
               <p class="text-xl font-semibold text-gray-900">{{ $b->judul }}</p>
               <p class="mt-3 text-base text-gray-500">{!! $b->deskripsi !!}</p>
-           <a href="#">Read More....</a>
+           <a href="/allbooks" class="text-gray-700 hover:text-amber-900">Read More....</a>
           </div>
           <div class="mt-6 flex items-center">
             <div class="ml-3">
               <p class="text-sm font-medium text-gray-900">
-                <a href="#" class="hover:underline"> {{ $b->penulis }} </a>
+                <p class=""> {{ $b->penulis }} </p>
               </p>
               <div class="flex space-x-1 text-sm text-gray-500">
                 <time datetime="2020-03-16"> {{ $b->tahun_terbit }} </time>
